0bab11b99078c4f94eb8ecfb602a6fc5b1bb587e
Sanek9010 <Sanek8005@gmail.com>
2017-12-22 17:58:49 +0400

    221217 alpha 0.1

--- src/DataBaseBundle/Controller/SecurityController.php
+++ src/DataBaseBundle/Controller/SecurityController.php
@@ -26,34 +26,16 @@ class SecurityController extends Controller
         ));
 
     }
-    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    public function checkAction(Request $request, AuthenticationUtils $authUtils)
     {
-        // 1) build the form
-        $user = new User();
-        $form = $this->createForm(UserLogin::class, $user);
 
-        // 2) handle the submit (will only happen on POST)
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
 
-            // 3) Encode the password (you could also do this via Doctrine listener)
-            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
-            $user->setPassword($password);
+        $token = $this->get('security.token_storage')->getToken();
 
-            // 4) save the User!
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+        $user = $token->getUser();
 
-            // ... do any other work - like sending them an email, etc
-            // maybe set a "flash" success message for the user
-
-            return $this->redirectToRoute('homepage2');
-        }
-
-        return $this->render(
-            '@DataBase/registration/register.html.twig',
-            array('form' => $form->createView())
-        );
+        return $this->render('@DataBase/atom/currentUser.html.twig', array(
+            'last_username' => $user->getUsername(),
+        ));
     }
 }
